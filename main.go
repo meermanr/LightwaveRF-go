3b9fa2688a39d9c3a814233ad373ee5fe21ad5df
Robert Meerman <robert.meerman@gmail.com>
2024-09-09 21:41:36 +0000

    Rewrite Dedent and add some tests

--- main.go
+++ main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"bytes"
 	"fmt"
 	"net"
 	"os"
@@ -14,80 +13,44 @@ const lwl_server_port = 9760 // We send to this address ...
 const lwl_client_port = 9761 // ... and listen for responses on this one
 
 // Initially we will use broadcasts to find the Lightwave Link. Once found we
-// will change this to the found the unicast address.
+// will change this to the found unicast address.
 var lwl_ip_address = "255.255.255.255"
-var lwl_mac_address string = "" // To be discovered at runtime
-var sid = 0                     // Sequence ID
+var lwl_mac_address = "" // To be discovered at runtime
+var sid = 0              // Sequence ID
 
 var state_tmpl *template.Template
 
-// Dedent removes indentation from a multi-line string.
-// The indentation level is determined automatically by counting the number of
-// whitespace characters on the first non-empty line.
-//
-// If the first line is empty, it is not emitted. If the last line is empty, it
-// is not emitted. This is convenient when using string literals, e.g.:
-//
-// Dedent(`
-//
-//	Golang is the best
-//	Who needs generics or maps?
-//	Just use interfaces
-//	`)
-func Dedent(in string) string {
+// Remove leading whitespace from every line of a string. The amount of
+// whitespace is calculated from the first (non-blank) line.
+func Dedent(in string) (out string) {
 	var indent string
-	var line string
-	var remaining string = in
-	var buf bytes.Buffer
-	var found bool
+	var lines []string
 
-	// Special case: Remove first character if it is a newline
-	if len(in) > 1 && in[0] == '\n' {
+	// Remove exactly one leading newline, if present
+	if len(in) >= 1 && in[0] == '\n' {
 		in = in[1:]
 	}
 
-IndentScanner:
-	for {
-		// Get next line
-		line, remaining, _ = strings.Cut(remaining, "\n")
-
-		// Skip empty lines
-		if line == "" {
+	// Determine indent of first line by scanning until first non-blank
+FindIndent:
+	for i, c := range in {
+		switch {
+		case c == ' ', c == '\t':
 			continue
+		default:
+			indent = in[:i]
+			break FindIndent
 		}
+	}
 
-		// Scan this non-empty for leading whitespace
-		for i := range len(line) {
-			c := line[i]
-			switch c {
-			case ' ', '\t':
-				continue
-			default:
-				// First rune that isn't whitespace
-				indent = line[:i]
-				break IndentScanner
-			}
-		}
+	// Strip indent from remaining lines
+	for _, s := range strings.Split(in, "\n") {
+		line := strings.TrimPrefix(s, indent)
+		lines = append(lines, line)
 	}
-	// fmt.Printf("Indent is %#v, length %d\n", indent, len(indent))
-	// Make a copy of in without indentation
-	remaining = in
-	for remaining != "" {
-		line, remaining, found = strings.Cut(remaining, "\n")
-		line, _ = strings.CutPrefix(line, indent)
-		// fmt.Printf("line: %#v, found: %v, remaining: %#v\n", line, found, remaining)
-		if _, err := buf.WriteString(line); err != nil {
-			panic(err)
-		}
-		// Special case: Omit last line if it is empty (after stripping indent)
-		// i.e. do not process the final line
-		if found && remaining != indent {
-			if _, err := buf.WriteRune('\n'); err != nil {
-				panic(err)
-			}
-		}
-	}
-	return buf.String()
+
+	out = strings.Join(lines, "\n")
+	return
 }
 
 func init() {
---
+++ main_test.go
@@ -0,0 +1,84 @@
+package main
+
+import "testing"
+
+func TestDedent(t *testing.T) {
+	type args struct {
+		in string
+	}
+	tests := []struct {
+		name    string
+		args    args
+		wantOut string
+	}{
+		{
+			name:    "Empty",
+			args:    args{``},
+			wantOut: ``,
+		},
+		{
+			name: "SingleBlank",
+			args: args{`
+`},
+			wantOut: ``,
+		},
+		{
+			name: "TwoBlank",
+			args: args{`
+
+`},
+			wantOut: `
+`,
+		},
+		{
+			name: "ThreeBlank",
+			args: args{`
+
+
+`},
+			wantOut: `
+
+`,
+		},
+		{
+			name:    "OneFlat",
+			args:    args{`No indent here`},
+			wantOut: `No indent here`,
+		},
+		{
+			name: "TwoFlat",
+			args: args{`
+No indent here`},
+			wantOut: `No indent here`,
+		},
+		{
+			name: "SingleIndent",
+			args: args{`
+	No indent here`},
+			wantOut: `No indent here`,
+		},
+		{
+			name: "SingleIndentB",
+			args: args{`
+		No indent here`},
+			wantOut: `No indent here`,
+		},
+		{
+			name: "TwoIndentA",
+			args: args{`
+		Some indent
+		
+		Another line`},
+			wantOut: `Some indent
+
+Another line`,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if gotOut := Dedent(tt.args.in); gotOut != tt.wantOut {
+				t.Errorf("Dedent() = %v, want %v", gotOut, tt.wantOut)
+			}
+		})
+	}
+}
